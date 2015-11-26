9e8a74aa0b0791e55d78de1744c40ed6d505ae18
Ognian Kostadinov <o.kostadinov@proxiad.com>
2015-11-26 18:57:42 +0200

    Handling null values in email validator

--- src/main/java/bg/proxiad/demo/championship/front/EmailValidator.java
+++ src/main/java/bg/proxiad/demo/championship/front/EmailValidator.java
@@ -1,5 +1,6 @@
 package bg.proxiad.demo.championship.front;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,8 +19,12 @@ public class EmailValidator {
 		pattern = Pattern.compile(EMAIL_PATTERN);
 	}
 
-	public boolean valid(final String email) {
-
+	public boolean valid(String email) {
+                
+                if(Objects.equals(email, null)) {
+                    return false;
+                }
+                
 		matcher = pattern.matcher(email);
 		return matcher.matches();
 
