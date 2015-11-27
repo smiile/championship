32d9483a1e2a935a18e2894278da5e760a70536b
Ognian Kostadinov <o.kostadinov@proxiad.com>
2015-11-27 12:10:13 +0200

    Finished mockito tests for participants

--- src/test/java/bg/proxiad/demo/championhip/mockito/StandaloneParticipantControllerTest.java
+++ src/test/java/bg/proxiad/demo/championhip/mockito/StandaloneParticipantControllerTest.java
@@ -6,7 +6,6 @@ import bg.proxiad.demo.championship.front.ParticipantsController;
 import bg.proxiad.demo.championship.front.WebAnnotationConfig;
 import bg.proxiad.demo.championship.logic.ParticipantService;
 import bg.proxiad.demo.championship.model.Participant;
-import bg.proxiad.demo.championship.viewbeans.ParticipantViewBean;
 import java.util.Arrays;
 
 import org.junit.Before;
@@ -14,30 +13,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.hamcrest.Matchers.is;
-import org.mockito.Mock;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -165,7 +152,56 @@ public class StandaloneParticipantControllerTest {
                 .andExpect(model().attributeHasFieldErrors("participantForm", "email"))
                 .andExpect(model().attributeHasFieldErrors("participantForm", "photoFileName")
         );
-
-                    
+    }
+    
+    @Test
+    public void saveOrUpdateParticipant_shouldShowErrorOnInvalidEmail() throws Exception {
+        mockMvc.perform(post("/participants")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .param("email", "invalid@email"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("participant/create-edit"))
+                .andExpect(model().attributeHasFieldErrors("participantForm", "email"))
+                .andExpect(model().attributeHasFieldErrorCode("participantForm", "email", "Pattern.participantForm.email")
+        );
+    }
+    
+    @Test
+    public void saveOrUpdateParticipant_shouldCreateNewParticipantAndRedirectToList() throws Exception {
+        
+        mockMvc.perform(post("/participants")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .param("email", "valid@email.com")
+                    .param("firstName", "first name")
+                    .param("lastName", "last name")
+                    .param("photoFileName", "participant.jpeg"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/app/participants"))
+                .andExpect(flash().attribute("css", is("success")))
+                .andExpect(flash().attribute("msg", is("Participant created successfully!"))
+        );
+    }
+    
+    @Test
+    public void saveOrUpdateParticipant_shouldUpdateParticpantAndRedirectToList() throws Exception {
+        Participant participant = new Participant();
+        participant.setId(1L);
+        
+        // Mocking the loading of real entity in the transformer-helper
+        when(participantServiceMock.loadParticipant(1L)).thenReturn(participant);
+        
+        mockMvc.perform(post("/participants")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .param("id", "1")
+                    .param("email", "valid@email.com")
+                    .param("firstName", "first name")
+                    .param("lastName", "last name")
+                    .param("photoFileName", "participant.jpeg"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/app/participants"))
+                .andExpect(flash().attribute("css", is("success")))
+                .andExpect(flash().attribute("msg", is("Participant saved successfully!"))
+        );
+        
     }
 }
