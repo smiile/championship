f4085b6f1b353001ef280ce39f613000473edfef
Ognian Kostadinov <o.kostadinov@proxiad.com>
2015-12-03 10:23:28 +0200

    Moved out the ranking logic to its service

--- src/main/java/bg/proxiad/demo/championship/logic/ParticipantResultService.java
+++ src/main/java/bg/proxiad/demo/championship/logic/ParticipantResultService.java
@@ -3,6 +3,7 @@ package bg.proxiad.demo.championship.logic;
 import bg.proxiad.demo.championship.model.Grouping;
 import bg.proxiad.demo.championship.model.ParticipantResult;
 import java.util.Collection;
+import java.util.List;
 
 public interface ParticipantResultService {
     
@@ -11,4 +12,6 @@ public interface ParticipantResultService {
     void saveOrUpdateParticipantResult(ParticipantResult participantResult);
     
     Collection<ParticipantResult> listAllParticipantResult();
+    
+    public List<ParticipantResult> calculateAndSetParticipantResultsPerGroup(Grouping group);
 }
--- src/main/java/bg/proxiad/demo/championship/logic/ParticipantResultServiceImpl.java
+++ src/main/java/bg/proxiad/demo/championship/logic/ParticipantResultServiceImpl.java
@@ -1,9 +1,14 @@
 package bg.proxiad.demo.championship.logic;
 
 import bg.proxiad.demo.championship.model.Grouping;
+import bg.proxiad.demo.championship.model.Match;
 import bg.proxiad.demo.championship.model.ParticipantResult;
 import bg.proxiad.demo.championship.model.ParticipantResultDao;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +19,9 @@ public class ParticipantResultServiceImpl implements ParticipantResultService {
 
     @Autowired
     ParticipantResultDao participantResultDao;
+    
+    @Autowired
+    MatchService matchService;
     
     
     @Override
@@ -29,6 +37,60 @@ public class ParticipantResultServiceImpl implements ParticipantResultService {
     @Override
     public void deleteAllParticipantResultByGroup(Grouping group) {
         participantResultDao.deleteAllByGroup(group);
+    }
+    
+    // Generates a "ranking" for each group and returns it as a list
+    @Override
+    public List<ParticipantResult> calculateAndSetParticipantResultsPerGroup(Grouping group) {
+        List<Match> groupMatches = matchService.listGroupMatches(group);
+        
+        HashMap<Long, ParticipantResult> hMap = new HashMap<>();
+        
+        // Accumulate results
+        for(Match match : groupMatches) {
+            ParticipantResult participantResultP1;
+            ParticipantResult participantResultP2;
+            
+            if(hMap.containsKey(match.getParticipant1().getId())) {
+                participantResultP1 = hMap.get(match.getParticipant1().getId());
+            } else {
+                participantResultP1 = new ParticipantResult();
+            }
+
+            participantResultP1.setPoints(participantResultP1.getPoints() + match.getP1GamesWon().intValue());
+            participantResultP1.setGroup(group);
+            participantResultP1.setParticipant(match.getParticipant1());
+            hMap.put(match.getParticipant1().getId(), participantResultP1);
+
+            if(hMap.containsKey(match.getParticipant2().getId())) {
+                participantResultP2 = hMap.get(match.getParticipant2().getId());
+            } else {
+                participantResultP2 = new ParticipantResult();
+            }
+
+            participantResultP2.setPoints(participantResultP2.getPoints() + match.getP2GamesWon().intValue());
+            participantResultP2.setGroup(group);
+            participantResultP2.setParticipant(match.getParticipant2());
+            hMap.put(match.getParticipant2().getId(), participantResultP2);
+        }
+        
+        List<ParticipantResult> participantResults = new ArrayList(hMap.values());
+        
+        // Sort results
+        Collections.sort(participantResults);
+        
+        // Delete previous participant results
+        this.deleteAllParticipantResultByGroup(group);
+        
+        // Set positions and save entities
+        for(int i = 0; i < participantResults.size(); i++) {
+            ParticipantResult tmp = participantResults.get(i);
+            tmp.setPosition(i+1);
+            
+//            participantResultService.saveOrUpdateParticipantResult(tmp); // Should be unnecesary in the service
+        }
+        
+        return participantResults;
     }
 
 }
